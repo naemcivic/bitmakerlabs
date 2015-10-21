b24b36caaa0d272fb4cd9540c99e168654206722
Philip Chan <philipchan@Philips-MacBook-Pro.local>
2015-10-21 11:19:11 -0400

    My 2nd commit o this file

--- favoritecities.rb
+++ favoritecities.rb
@@ -1,3 +1,4 @@
+#This will iterate and print out to screen my favorite cities
 favorite_cities = ["Quebec City", "Montreal", "Toronto", "Hong Kong", "Ottawa"]
 puts "The following are my 5 favorite cities:"
 favorite_cities.each {|x| puts x}
